df4bcc33b7e84a250efe469c6138988636ab34fc
Collin Brittle <cnbrittle@gmail.com>
2015-05-21 16:30:48 -0400

    Rewrite bootstrap.sh
    Separate fast-changing version from the script, put 'em in variables.
    Re-add `sudo` where needed, so the script does not need to be run as root.
    Accept the name of the hydra head and the git repository as arguments.
    Remove configuration changes that are supplied through the git repository.

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -5,30 +5,36 @@ set -o errexit
 # Installs the default Sufia application and all of it's dependencies.
 
 # Vars
-fitsdir="$HOME/fits" # Where FITS will be installed.
-fitsver="fits-0.6.2" # Which version of FITS to install.
-hydrahead="data-repo" # Name of the Hydra head.
-hydradir="$HOME/$hydrahead" # Where the Hydra head will be located.
+basedir="/home/vagrant"
+fitsdir="$basedir/fits" # Where FITS will be installed.
+hydrahead="$1" # Name of the Hydra head. Supplied by the Vagrantfile
+hydradir="$basedir/$hydrahead" # Where the Hydra head will be located.
+gitrepo="$2" # The git repository to pull changes from during setup. Supplied by the Vagrantfile.
+
+fitsver="fits-0.6.2" # The version of FITS to install.
+rubyver="ruby2.2" # The version of Ruby to install.
+railsver=">=4.2" # The version of Rails to install.
+sufiaver="6.0.0" # The version of Sufia to install.
 
 # Update packages
-cd ~
+cd "$basedir/"
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# Install Ruby 2.2
+# Install Ruby
 # Brightbox also packages Passenger, which will be useful for production.
 sudo add-apt-repository -y ppa:brightbox/ruby-ng
 sudo apt-get update
-sudo apt-get install -y ruby2.2
+sudo apt-get install -y "$rubyver"
 
 # Install FITS
 sudo apt-get install -y openjdk-7-jdk unzip
 mkdir -p "$fitsdir/"
 cd "$fitsdir/"
-wget "http://projects.iq.harvard.edu/files/fits/files/$fitsver.zip"
-unzip "$fitsdir/$fitsver.zip"
+wget --quiet "http://projects.iq.harvard.edu/files/fits/files/$fitsver.zip"
+unzip -q "$fitsdir/$fitsver.zip"
 sudo chmod a+x "$fitsdir/$fitsver/fits.sh"
-cd "$HOME/"
+cd "$basedir/"
 
 # Install ffmpeg
 # Instructions from the static builds link on this page: https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu
@@ -43,14 +49,14 @@ sudo apt-get install -y nodejs
 # Install Redis, ImageMagick, PhantomJS, and Libre Office
 sudo apt-get install -y redis-server imagemagick phantomjs libreoffice
 
-# Create Hydra head.
-sudo apt-get install -y ruby2.2-dev git sqlite3 libsqlite3-dev zlib1g-dev build-essential
-sudo gem install --no-document rails -v ">=4.2"
+# Create Hydra head
+sudo apt-get install -y "$rubyver-dev" git sqlite3 libsqlite3-dev zlib1g-dev build-essential
+sudo gem install --no-document rails -v "$railsver"
 rails new "$hydrahead" "$hydradir"
 
 # Add and set up Sufia
-cd "$hydradir"
-echo "gem 'sufia', '6.0.0'" >> "$hydradir/Gemfile"
+cd "$hydradir/"
+echo "gem 'sufia', '$sufiaver'" >> "$hydradir/Gemfile"
 echo "gem 'kaminari', github: 'jcoyne/kaminari', branch: 'sufia'" >> "$hydradir/Gemfile"
 bundle install
 rails generate sufia:install -f
@@ -59,16 +65,9 @@ bundle exec rake db:migrate
 # Download and configure Jetty
 bundle exec rake jetty:clean sufia:jetty:config
 
-# Fix Hydra head configs.
-# Point to FITS at our location.
-sed --in-place=".bak" --expression="s|# config.fits_path = \"fits.sh\"|config.fits_path = \"$fitsdir/$fitsver/fits.sh\"|" \
-"$hydradir/config/initializers/sufia.rb"
-# Remove turbolinks.
-sed --in-place=".bak" --expression="/\/\/= require turbolinks/ d" "$hydradir/app/assets/javascripts/application.js"
-
-# Pull from git
-cd "$hydradir"
+# Pull from git. This fixes application configuration 
 git init
-git remote add origin https://github.com/VTUL/data-repo.git
+git remote add origin "https://github.com/$gitrepo.git"
 git fetch --all
 git reset --hard origin/master
+bundle install
