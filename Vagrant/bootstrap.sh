20924b9091073d1b151744bb600a1a6688dc90e0
Paul Mather <pmather@vt.edu>
2015-07-08 21:28:43 -0400

    Run bootstrap script as root.
    The bootstrap.sh script is now run as a privileged user, i.e., root.
    This is to make it easier to use the same script for bootstrapping AWS
    instances, which run bootstrapping scripts as root.
    
    Commands that previously required "sudo" to run them as root have had
    the "sudo" removed.  Likewise, commands that previously ran as the
    unprivileged user are run via "sudo," specifying the unprivileged user
    as the one to run under.
    
    The "-H' flag is used in the sudo command to ensure $HOME is set
    correctly.  This prevents problems with Bundler, which stores things
    under $HOME/.bundler.  Without the "-H", Bundler would fail trying to
    create files under root's home directory.

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -28,58 +28,60 @@ rubyver="ruby2.2" # The version of Ruby to install.
 railsver=">=4.2" # The version of Rails to install.
 sufiaver="6.0.0" # The version of Sufia to install.
 
+RUN_AS_INSTALLUSER="sudo -H -u $installuser"
+
 # Update packages
 cd "$basedir/"
-sudo apt-get update
-sudo apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
 
 # Install Ruby
 # Brightbox also packages Passenger, which will be useful for production.
-sudo add-apt-repository -y ppa:brightbox/ruby-ng
-sudo apt-get update
-sudo apt-get install -y "$rubyver"
+add-apt-repository -y ppa:brightbox/ruby-ng
+apt-get update
+apt-get install -y "$rubyver"
 
 # Install FITS
-sudo apt-get install -y openjdk-7-jdk unzip
-mkdir -p "$fitsdir/"
+apt-get install -y openjdk-7-jdk unzip
+$RUN_AS_INSTALLUSER mkdir -p "$fitsdir/"
 cd "$fitsdir/"
-wget --quiet "http://projects.iq.harvard.edu/files/fits/files/$fitsver.zip"
-unzip -q "$fitsdir/$fitsver.zip"
-sudo chmod a+x "$fitsdir/$fitsver/fits.sh"
+$RUN_AS_INSTALLUSER wget --quiet "http://projects.iq.harvard.edu/files/fits/files/$fitsver.zip"
+$RUN_AS_INSTALLUSER unzip -q "$fitsdir/$fitsver.zip"
+chmod a+x "$fitsdir/$fitsver/fits.sh"
 cd "$basedir/"
 
 # Install ffmpeg
 # Instructions from the static builds link on this page: https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu
-sudo add-apt-repository -y ppa:mc3man/trusty-media
-sudo apt-get update
-sudo apt-get install -y ffmpeg
+add-apt-repository -y ppa:mc3man/trusty-media
+apt-get update
+apt-get install -y ffmpeg
 
 # Install nodejs from Nodesource
-curl -sL https://deb.nodesource.com/setup | sudo bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup | bash -
+apt-get install -y nodejs
 
 # Install Redis, ImageMagick, PhantomJS, and Libre Office
-sudo apt-get install -y redis-server imagemagick phantomjs libreoffice
+apt-get install -y redis-server imagemagick phantomjs libreoffice
 
 # Create Hydra head
-sudo apt-get install -y "$rubyver-dev" git sqlite3 libsqlite3-dev zlib1g-dev build-essential
-sudo gem install --no-document rails -v "$railsver"
-rails new "$hydrahead" "$hydradir"
+apt-get install -y "$rubyver-dev" git sqlite3 libsqlite3-dev zlib1g-dev build-essential
+gem install --no-document rails -v "$railsver"
+$RUN_AS_INSTALLUSER rails new "$hydrahead" "$hydradir"
 
 # Add and set up Sufia
 cd "$hydradir/"
-echo "gem 'sufia', '$sufiaver'" >> "$hydradir/Gemfile"
-echo "gem 'kaminari', github: 'jcoyne/kaminari', branch: 'sufia'" >> "$hydradir/Gemfile"
-bundle install
-rails generate sufia:install -f
-bundle exec rake db:migrate
+$RUN_AS_INSTALLUSER echo "gem 'sufia', '$sufiaver'" >> "$hydradir/Gemfile"
+$RUN_AS_INSTALLUSER echo "gem 'kaminari', github: 'jcoyne/kaminari', branch: 'sufia'" >> "$hydradir/Gemfile"
+$RUN_AS_INSTALLUSER bundle install
+$RUN_AS_INSTALLUSER rails generate sufia:install -f
+$RUN_AS_INSTALLUSER bundle exec rake db:migrate
 
 # Download and configure Jetty
-bundle exec rake jetty:clean sufia:jetty:config
+$RUN_AS_INSTALLUSER bundle exec rake jetty:clean sufia:jetty:config
 
 # Pull from git. This fixes application configuration
-git init
-git remote add origin "https://github.com/$gitrepo.git"
-git fetch --all
-git reset --hard origin/master
-bundle install
+$RUN_AS_INSTALLUSER git init
+$RUN_AS_INSTALLUSER git remote add origin "https://github.com/$gitrepo.git"
+$RUN_AS_INSTALLUSER git fetch --all
+$RUN_AS_INSTALLUSER git reset --hard origin/master
+$RUN_AS_INSTALLUSER bundle install
