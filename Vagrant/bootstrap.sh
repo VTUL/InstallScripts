cb1c2f29d94142a99175a9b779673702e871779c
Paul Mather <pmather@vt.edu>
2015-07-08 20:37:10 -0400

    Pass install user into bootstrap and runserver scripts.
    Remove hard-coding of "vagrant" user and, instead, pass it in to the
    bootstrap.sh and runserver scripts.
    
    The bootstrap.sh script now expects these arguments:
    - User under which to install application.  This user must already
    exist and is expected to have a home directory under /home;
    - Name of Hydra head;
    - Name of GitHub repo containing Hydra head application
    
    The bootstrap.sh script contains defaults suitable for AWS.  The
    Vagrantfile overrides these with values suitable for Vagrant.
    
    The runserver script now takes the install user as its first argument
    and the name of the Hydra head as its second.

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -6,11 +6,22 @@ set -o errexit
 echo "Version 1.0"
 
 # Vars
-basedir="/home/vagrant"
+installuser="ubuntu" # Name of user to install under (must already exist)
+hydrahead="data-repo" # Name of the Hydra head.
+gitrepo="VTUL/data-repo" # The git repository to pull changes from during setup.
+# Override installuser, hydrahead, and gitrepo via shell script arguments
+if [ $# -ge 1 ]; then
+  installuser="$1"
+fi
+if [ $# -ge 2 ]; then
+  hydrahead="$2"
+fi
+if [ $# -ge 3 ]; then
+  gitrepo="$3"
+fi
+basedir="/home/$installuser"
 fitsdir="$basedir/fits" # Where FITS will be installed.
-hydrahead="$1" # Name of the Hydra head. Supplied by the Vagrantfile
 hydradir="$basedir/$hydrahead" # Where the Hydra head will be located.
-gitrepo="$2" # The git repository to pull changes from during setup. Supplied by the Vagrantfile.
 
 fitsver="fits-0.6.2" # The version of FITS to install.
 rubyver="ruby2.2" # The version of Ruby to install.
@@ -66,7 +77,7 @@ bundle exec rake db:migrate
 # Download and configure Jetty
 bundle exec rake jetty:clean sufia:jetty:config
 
-# Pull from git. This fixes application configuration 
+# Pull from git. This fixes application configuration
 git init
 git remote add origin "https://github.com/$gitrepo.git"
 git fetch --all
