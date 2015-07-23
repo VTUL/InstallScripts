cf3e4873c006bf9f171320ad8c8cc7736ec680e9
Paul Mather <pmather@vt.edu>
2015-07-23 14:13:41 -0400

    Run application via Nginx/Passenger by default.
    This change moves the CAS_extras.sh steps into the main bootstrap.sh
    install script.  This means the Sufia application will be run via
    Nginx/Passenger rather than the WEBrick server used by "rails server."
    The other significant change is that now all the installation steps are
    automated: there is no need to run CAS_extras.sh manually after the
    initial provisioning.
    
    This commit includes these specific changes:
    
     - The version of Sufia has been bumped to 6.2.0.
    
     - The WEBrick port is no longer exposed to the host system in Vagrant.
    
     - The Fedora/Solr port (8983) is exposed to the host system in Vagrant.
    
     - The Web server port mappings are changed in Vagrant.  They are now
    8080 -> 80 and 4443 -> 443.  This is because the Web server now runs on
    standard ports on the VM/server.
    
     - Because it has been incorporated into bootstrap.sh, the
    CAS_extras.sh script has been removed.
    
     - The bootstrap.sh script now takes a fourth argument: the hostname of
    the Web server site (default: "localhost").
    
     - Nginx and Passenger are installed via the Phusion Passenger APT
    repository as part of the main bootstrap sequence.
    
     - Additionally, a Web site is configured that runs the Sufia
    application via Passenger.
    
     - A self-signed SSL key is generated and installed for the
    aforementioned Web site, allowing HTTPS access.
    
     - The Sufia application is deployed in production mode as part of the
    main bootstrap sequence instead of via CAS_extras.sh.
    
     - Instructions are no longer issued to the user to generate a SSL
    certificate and restart Nginx.
    
     - The "run always" Vagrant provisioner now also restarts Nginx, in
    case it is not running.

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -9,7 +9,8 @@ echo "Version 1.0"
 installuser="ubuntu" # Name of user to install under (must already exist)
 hydrahead="data-repo" # Name of the Hydra head.
 gitrepo="VTUL/data-repo" # The git repository to pull changes from during setup.
-# Override installuser, hydrahead, and gitrepo via shell script arguments
+SERVER_HOSTNAME="localhost"
+# Override installuser, hydrahead, gitrepo, and SERVER_HOSTNAME via shell script arguments
 if [ $# -ge 1 ]; then
   installuser="$1"
 fi
@@ -19,14 +20,25 @@ fi
 if [ $# -ge 3 ]; then
   gitrepo="$3"
 fi
+if [ $# -ge 4 ]; then
+  SERVER_HOSTNAME="$4"
+fi
 basedir="/home/$installuser"
 fitsdir="$basedir/fits" # Where FITS will be installed.
 hydradir="$basedir/$hydrahead" # Where the Hydra head will be located.
+PASSENGER_REPO="/etc/apt/sources.list.d/passenger.list"
+NGINX_CONF_DIR="/etc/nginx"
+NGINX_CONF_FILE="$NGINX_CONF_DIR/nginx.conf"
+NGINX_SITE="$NGINX_CONF_DIR/sites-available/$hydrahead.site"
+SSL_CERT_DIR="/etc/ssl/local/certs"
+SSL_CERT="$SSL_CERT_DIR/$hydrahead-crt.pem"
+SSL_KEY_DIR="/etc/ssl/local/private"
+SSL_KEY="$SSL_KEY_DIR/$hydrahead-key.pem"
 
 fitsver="fits-0.6.2" # The version of FITS to install.
 rubyver="ruby2.2" # The version of Ruby to install.
 railsver=">=4.2" # The version of Rails to install.
-sufiaver="6.0.0" # The version of Sufia to install.
+sufiaver="6.2.0" # The version of Sufia to install.
 
 RUN_AS_INSTALLUSER="sudo -H -u $installuser"
 
@@ -35,11 +47,10 @@ cd "$basedir/"
 apt-get update
 apt-get upgrade -y
 
-# Install Ruby
-# Brightbox also packages Passenger, which will be useful for production.
+# Install Ruby via Brightbox repository
 add-apt-repository -y ppa:brightbox/ruby-ng
 apt-get update
-apt-get install -y "$rubyver"
+apt-get install -y "$rubyver" "$rubyver-dev"
 
 # Install Java 8 and make it the default Java
 add-apt-repository -y ppa:webupd8team/java
@@ -70,8 +81,68 @@ apt-get install -y nodejs
 # Install Redis, ImageMagick, PhantomJS, and Libre Office
 apt-get install -y redis-server imagemagick phantomjs libreoffice
 
+# Install Nginx and Passenger.
+# Install the Phusion Passenger APT repository
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
+#sudo apt-get install apt-transport-https ca-certificates # Not necessary for 14_04, but part of the Phusion Docs.
+echo "deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main" > $PASSENGER_REPO
+chown root: $PASSENGER_REPO
+chmod 600 $PASSENGER_REPO
+apt-get update
+# Install Nginx and Passenger
+apt-get install -y nginx-extras passenger
+# Uncomment passenger_root and passenger_ruby lines from config file
+TMPFILE=`/bin/mktemp`
+cat $NGINX_CONF_FILE | \
+  sed "s/# passenger_root/passenger_root/" | \
+  sed "s/# passenger_ruby/passenger_ruby/" > $TMPFILE
+sed "1ienv PATH;" < $TMPFILE > $NGINX_CONF_FILE
+chown root: $NGINX_CONF_FILE
+chmod 644 $NGINX_CONF_FILE
+# Disable the default site
+unlink "$NGINX_CONF_DIR/sites-enabled/default"
+# Stop Nginx until the application is installed
+service nginx stop
+
+# Configure Passenger to serve our site.
+# Create the virtual host for our Sufia application
+cat > $TMPFILE <<HereDoc
+server {
+    listen 80;
+    listen 443 ssl;
+    root $hydradir/public;
+    passenger_enabled on;
+    server_name $SERVER_HOSTNAME;
+    ssl_certificate $SSL_CERT;
+    ssl_certificate_key $SSL_KEY;
+}
+HereDoc
+# Install the virtual host config as an available site
+install -o root -g root -m 644 $TMPFILE $NGINX_SITE
+rm $TMPFILE
+# Enable the site just created
+link $NGINX_SITE "$NGINX_CONF_DIR/sites-enabled/$hydrahead.site"
+# Create the directories for the SSL certificate files
+mkdir -p $SSL_CERT_DIR
+mkdir -p $SSL_KEY_DIR
+# Create self-signed certificate (NB: line breaks are significant)
+SUBJECT="
+C=US
+ST=Virginia
+O=Virginia Tech
+localityName=Blacksburg
+commonName=$SERVER_HOSTNAME
+organizationalUnitName=University Libraries
+emailAddress=
+"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $SSL_KEY \
+  -out $SSL_CERT -subj "$(echo -n "$SUBJECT" | tr "\n" "/")"
+chmod 444 $SSL_CERT
+chown root $SSL_KEY
+chmod 400 $SSL_KEY
+
 # Create Hydra head
-apt-get install -y "$rubyver-dev" git sqlite3 libsqlite3-dev zlib1g-dev build-essential
+apt-get install -y git sqlite3 libsqlite3-dev zlib1g-dev build-essential
 gem install --no-document rails -v "$railsver"
 $RUN_AS_INSTALLUSER rails new "$hydrahead" "$hydradir"
 
@@ -126,3 +197,18 @@ $RUN_AS_INSTALLUSER bundle exec rake datarepo:setup_defaults
 $RUN_AS_INSTALLUSER bundle exec rails generate orcid:install --skip-application-yml
 # 8. Revert changes already incorporated
 $RUN_AS_INSTALLUSER git checkout ./app/models/user.rb ./config/routes.rb
+
+# Application Deployment steps.
+cd "$hydradir"
+$RUN_AS_INSTALLUSER bundle install
+$RUN_AS_INSTALLUSER rails g migration AddOmniauthToUsers provider uid
+$RUN_AS_INSTALLUSER rake db:migrate
+$RUN_AS_INSTALLUSER bundle install --deployment --without development test
+$RUN_AS_INSTALLUSER sed --in-place=".bak" --expression="s|<%= ENV\[\"SECRET_KEY_BASE\"\] %>|$(bundle exec rake secret)|" "$hydradir/config/secrets.yml"
+$RUN_AS_INSTALLUSER RAILS_ENV=production bundle exec rake db:setup
+$RUN_AS_INSTALLUSER RAILS_ENV=production bundle exec rake assets:precompile
+$RUN_AS_INSTALLUSER RAILS_ENV=production bundle exec rake datarepo:setup_defaults
+$RUN_AS_INSTALLUSER bash $hydradir/scripts/restart_resque.sh production
+$RUN_AS_INSTALLUSER bundle exec rake jetty:start
+# Start Nginx
+service nginx start
