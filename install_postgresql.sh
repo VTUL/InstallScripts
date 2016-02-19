70a270575356ff6a2c104e35c64c5c8ff7fdbe85
Paul Mather <pmather@vt.edu>
2016-02-18 21:53:57 -0500

    Support local/remote PostgreSQL databases
    This change introduces a DB_IS_REMOTE configuration option to designate
    whether the PostgreSQL database server is running on a remote system or
    should be installed to run locally.
    
    If DB_IS_REMOTE="YES" then we skip installing the PostgreSQL server as
    part of the installation process.  Instead, we install only the
    PostgreSQL client applications and then use them to create the
    application database and user on the remote server.
    
    Otherwise, if DB_IS_REMOTE is not "YES" then it is assumed that a
    PostgreSQL server will be installed locally.
    
    When installing with DB_IS_REMOTE="YES", it is incumbent upon the
    person installing to supply an appropriately configured
    files/secrets.yml file.  In particular, the "host:" setting in the
    "database:" section of files/secrets.yml must be set accordingly for
    the installation to succeed.  (Otherwise, the db:schema:load task will
    fail during installation.)  This is in addition to specifying the
    appropriate DB_* configuration options in config.sh, etc.

--- install_postgresql.sh
+++ install_postgresql.sh
@@ -10,6 +10,29 @@ set -o errexit -o nounset -o xtrace -o pipefail
 
 cd "$INSTALL_DIR"
 
-apt-get install -y postgresql libpq-dev
-$POSTGRESQL_COMMAND psql -c "CREATE USER ${INSTALL_USER} WITH PASSWORD '${DB_PASS}';"
-$POSTGRESQL_COMMAND psql -c "CREATE DATABASE datarepo WITH OWNER ${INSTALL_USER} ENCODING 'UTF8';"
+apt-get install -y libpq-dev
+case $DB_IS_REMOTE in
+  [Yy][Ee][Ss])
+    echo "Assuming DB is on remote server."
+    apt-get install -y postgresql-client
+    PGPASSFILE=$(mktemp)
+    echo "${DB_HOST}:${DB_PORT}:${DB_ADMIN_DB}:${DB_ADMIN_USER}:${DB_ADMIN_PASS}" > "$PGPASSFILE"
+    chmod 0600 "$PGPASSFILE"
+    export PGHOST=$DB_HOST
+    export PGPORT=$DB_PORT
+    export PGUSER=$DB_ADMIN_USER
+    export PGDATABASE=$DB_ADMIN_DB
+    export PGPASSFILE
+    psql -c "DROP DATABASE IF EXISTS ${DB_NAME};"
+    psql -c "DROP USER IF EXISTS ${DB_USER};"
+    psql -c "CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASS}';"
+    psql -c "CREATE DATABASE ${DB_NAME} WITH OWNER ${DB_USER} ENCODING 'UTF8';"
+    rm "$PGPASSFILE"
+    ;;
+  *)
+    echo "Assuming DB is local."
+    apt-get install -y postgresql
+    sudo -u postgres psql -c "CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASS}';"
+    sudo -u postgres psql -c "CREATE DATABASE ${DB_NAME} WITH OWNER ${DB_USER} ENCODING 'UTF8';"
+    ;;
+esac
